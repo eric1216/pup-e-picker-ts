b109c8a4c0a0e2d635e20aeeecfbea6028fe0200
Eric H <ericolebusiness@gmail.com>
2024-07-12 20:21:31 -0400

    initial tab selection logic working (functional app)

--- src/Functional/FunctionalApp.tsx
+++ src/Functional/FunctionalApp.tsx
@@ -1,16 +1,20 @@
-import { FunctionalCreateDogForm } from "./FunctionalCreateDogForm";
-import { FunctionalDogs } from "./FunctionalDogs";
-import { FunctionalSection } from "./FunctionalSection";
+import { useState } from 'react';
+import { FunctionalCreateDogForm } from './FunctionalCreateDogForm';
+import { FunctionalDogs } from './FunctionalDogs';
+import { FunctionalSection } from './FunctionalSection';
 
 export function FunctionalApp() {
+  const [isUserCreatingDog, setIsUserCreatingDog] = useState<boolean>(false);
+
   return (
-    <div className="App" style={{ backgroundColor: "skyblue" }}>
+    <div className='App' style={{ backgroundColor: 'skyblue' }}>
       <header>
         <h1>pup-e-picker (Functional)</h1>
       </header>
-      <FunctionalSection />
-      <FunctionalDogs />
-      <FunctionalCreateDogForm />
+      <FunctionalSection setIsUserCreatingDog={setIsUserCreatingDog}>
+        {!isUserCreatingDog && <FunctionalDogs />}
+        {isUserCreatingDog && <FunctionalCreateDogForm />}
+      </FunctionalSection>
     </div>
   );
 }
--- src/Functional/FunctionalSection.tsx
+++ src/Functional/FunctionalSection.tsx
@@ -1,31 +1,65 @@
 // you can use this type for react children if you so choose
-import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export const FunctionalSection = () => {
+type Tabs = 'favorited' | 'unfavorited' | 'create' | null;
+
+export const FunctionalSection = ({
+  setIsUserCreatingDog,
+  children,
+}: {
+  setIsUserCreatingDog: Dispatch<SetStateAction<boolean>>;
+  children: ReactNode;
+}) => {
+  const [activeTab, setActiveTab] = useState<Tabs>(null);
+
+  const handleTabChange = (selectedTab: Tabs) => {
+    const newActiveTab = activeTab === selectedTab ? null : selectedTab;
+    setActiveTab(newActiveTab);
+  };
+
+  useEffect(() => {
+    setIsUserCreatingDog(activeTab === 'create');
+  }, [activeTab]);
+
   return (
-    <section id="main-section">
-      <div className="container-header">
-        <div className="container-label">Dogs: </div>
-        <Link to={"/class"} className="btn">
+    <section id='main-section'>
+      <div className='container-header'>
+        <div className='container-label'>Dogs: </div>
+        <Link to={'/class'} className='btn'>
           Change to Class
         </Link>
-        <div className="selectors">
+        <div className={'selectors'}>
           {/* This should display the favorited count */}
-          <div className={`selector active`} onClick={() => {}}>
+          <div
+            className={`selector ${activeTab === 'favorited' ? 'active' : ''}`}
+            onClick={() => {
+              handleTabChange('favorited');
+            }}
+          >
             favorited ( 12 )
           </div>
-
           {/* This should display the unfavorited count */}
-          <div className={`selector`} onClick={() => {}}>
+          <div
+            className={`selector ${activeTab === 'unfavorited' ? 'active' : ''}`}
+            onClick={() => {
+              handleTabChange('unfavorited');
+            }}
+          >
             unfavorited ( 25 )
           </div>
-          <div className={`selector`} onClick={() => {}}>
+          <div
+            className={`selector ${activeTab === 'create' ? 'active' : ''}`}
+            onClick={() => {
+              handleTabChange('create');
+              setIsUserCreatingDog(true);
+            }}
+          >
             create dog
           </div>
         </div>
       </div>
-      <div className="content-container"></div>
+      <div className='content-container'>{children}</div>
     </section>
   );
 };
